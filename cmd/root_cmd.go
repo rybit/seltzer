b781288f2d6e351e029c2c8fc4cef7776eabcbed
Ryan Neal <ry.neal@gmail.com>
2016-10-04 10:35:05 -0700

    rename project

--- cmd/root_cmd.go
+++ cmd/root_cmd.go
@@ -3,7 +3,7 @@ package cmd
 import (
 	"log"
 
-	"github.com/rybit/config_example/conf"
+	"github.com/rybit/seltzer/conf"
 	"github.com/spf13/cobra"
 )
 
--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"log"
 
-	"github.com/rybit/config_example/cmd"
+	"github.com/rybit/seltzer/cmd"
 )
 
 func main() {
